2c25cbc9fc27ace59d214abf6f348f2c57cd64d5
Todor Marta <todor.e.marta@gmail.com>
2023-01-24 18:02:42 -0300

    main-js-bug-fix

--- shopping-cart-js-starter-files/src/main.js
+++ shopping-cart-js-starter-files/src/main.js
@@ -3,8 +3,7 @@ let shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData
-    .map((x) => {
+  return (shop.innerHTML = shopItemsData.map((x) => {
       let { id, name, price, desc, img } = x;
       let search = basket.find((x) => x.id === id) || [];
       return `
